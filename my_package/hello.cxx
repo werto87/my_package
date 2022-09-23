8087dba6b0cade2b104d9a3a1715587769961016
werto87 <huhulasmichinruhe@web.de>
2022-09-23 13:45:44 +0900

    add a dependency

--- my_package/hello.cxx
+++ my_package/hello.cxx
@@ -1,7 +1,9 @@
 #include "hello.hxx"
-
+#include <boost/asio.hpp>
+#include <boost/asio/io_context.hpp>
 std::string
 helloWorld ()
 {
+  auto ioContext = boost::asio::io_context{};
   return "Hello World!";
 }
